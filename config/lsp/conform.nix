9f8b39010c46dc551ad1a7376b777d0685a15730
t.lettermann <theo.lettermann@gmail.com>
2024-10-08 17:42:30 +0200

    fix and add bash to conform

--- config/lsp/conform.nix
+++ config/lsp/conform.nix
@@ -1,20 +1,23 @@
 {
   plugins.conform-nvim = {
     enable = true;
-    notifyOnError = true;
-    formattersByFt = {
-      html = [["prettierd" "prettier"]];
-      css = [["prettierd" "prettier"]];
-      javascript = [["prettierd" "prettier"]];
-      javascriptreact = [["prettierd" "prettier"]];
-      typescript = [["prettierd" "prettier"]];
-      typescriptreact = [["prettierd" "prettier"]];
-      java = ["google-java-format"];
-      python = ["ruff"];
-      lua = ["stylua"];
-      nix = ["alejandra"];
-      markdown = [["prettierd" "prettier"]];
-      rust = ["rustfmt"];
+    settings = {
+      notify_on_error = true;
+      formatters_by_ft = {
+        html = [["prettierd" "prettier"]];
+        css = [["prettierd" "prettier"]];
+        javascript = [["prettierd" "prettier"]];
+        javascriptreact = [["prettierd" "prettier"]];
+        typescript = [["prettierd" "prettier"]];
+        typescriptreact = [["prettierd" "prettier"]];
+        java = ["google-java-format"];
+        python = ["ruff"];
+        lua = ["stylua"];
+        nix = ["alejandra"];
+        markdown = [["prettierd" "prettier"]];
+        rust = ["rustfmt"];
+        bash = ["shfmt"];
+      };
     };
   };
 
